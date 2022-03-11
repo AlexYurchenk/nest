df5177c7120bbb27c0467a9111b52996168b65a4
Oleksii Yurchenko <yurchenkoalex13@gmail.com>
2022-03-11 15:25:55 +0200

    nest models

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,23 @@
+export enum TopLevelCategory {
+  Books,
+  Products,
+  Courses,
+  Services,
+}
+
+export class TopPageModel {
+  firstCategory: TopLevelCategory;
+  secondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: { title: string; description: string }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
+}
